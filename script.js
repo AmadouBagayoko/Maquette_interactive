aa30c65154fed0973fb0c4fa5d175896a381ee28
Amadou Bagayoko <abagayoko304@gmail.com>
2025-04-09 15:19:31 +0000

    ajout de l'option telecharger et changer de couleurs

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function telecharger() {
+    const color= document.getElementById('color-cont');
+    color.style.visibility='hidden';
     const element = document.getElementById('cv');
-  
     const opt = {
       margin:       0,
-      filename:     'mon_cv.pdf',
+      filename:     'cv_amadou.pdf',
       image:        { type: 'jpeg', quality: 1 },
       html2canvas:  { 
         scale: 2,
@@ -11,10 +12,11 @@ function telecharger() {
       },
       jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
     };
-  
-    html2pdf().set(opt).from(element).save();
-  }
-  
+    html2pdf().set(opt).from(element).save().then(() => {
+      // Réafficher la palette après le téléchargement
+      color.style.visibility = 'visible';
+  });
+  }  
 //   fonction pour changer de couleur
 document.getElementById('color-picker').addEventListener('input', function() {
     const newColor = this.value;
